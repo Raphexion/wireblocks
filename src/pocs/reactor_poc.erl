f660fc270b59147268ef1e4e7c40202d481655cc
Niklas Johansson <raphexion@gmail.com>
2018-03-17 12:42:08 +0100

    improved reactor
    Signed-off-by: Niklas Johansson <raphexion@gmail.com>

--- src/pocs/reactor_poc.erl
+++ src/pocs/reactor_poc.erl
@@ -4,9 +4,17 @@
 
 start() ->
     {ok, Supervisor} = wireblocks_sup:start_link(),
-    {ok, _X} = wire:start(Supervisor, [x, 2]),
+    {ok, _X} = wire:start(Supervisor, [x, 0]),
 
     {ok, _Debug} = debug:start(debug0, x, no_local),
 
     {ok, _Reactor} = reactor:start_link(),
-    reactor:exec(x, 42, 100).
+    reactor:exec_ms(x, 5, 5000),
+    reactor:exec_ms(x, 4, 4000),
+    reactor:exec_ms(x, 3, 3000),
+    reactor:exec_ms(x, 9, 9000),
+    reactor:exec_ms(x, 1, 1000),
+    reactor:exec_ms(x, 8, 8000),
+    reactor:exec_ms(x, 7, 7000),
+    reactor:exec_ms(x, 6, 6000),
+    reactor:exec_ms(x, 2, 2000).
--- src/reactor.erl
+++ src/reactor.erl
@@ -3,11 +3,13 @@
 -behaviour(gen_server).
 
 -define(SERVER, ?MODULE).
+-define(DEFAULT_EMTPY_TICK, 100000).
 
 %%
 
 -export([start_link/0,
-	 exec/3]).
+	 exec_ms/3,
+	 exec_us/3]).
 
 -export([init/1,
 	 handle_call/3,
@@ -21,39 +23,32 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-exec(Wire, Value, Delay) ->
-    gen_server:cast(?MODULE, {exec, Wire, Value, Delay}).
+exec_ms(Wire, Value, DelayMs) ->
+    exec_us(Wire, Value, DelayMs * 1000).
+
+exec_us(Wire, Value, DelayUs) ->
+    ExecTime = DelayUs +  erlang:monotonic_time(microsecond),
+    gen_server:cast(?MODULE, {exec, Wire, Value, ExecTime}).
 
 %%
 
 init([]) ->
-    T0 = gb_trees:empty(),
-    {ok, {T0, 0}}.
+    Tree = gb_trees:empty(),
+    {ok, {Tree}}.
 
 handle_call(What, _From, State) ->
     {reply, {ok, What}, State}.
 
-handle_cast({exec, Wire, Value, Delay}, {Tree0, Time0}) ->
-    Tree1 = gb_trees:enter(Time0 + Delay,
-			   fun() -> wire:set(Wire, Value) end,
-			   Tree0),
-    {Time1, _} =  gb_trees:smallest(Tree1),
-    {noreply, {Tree1, Time1}, Time1 - Time0}.
+handle_cast({exec, Wire, Value, ExecTime}, {Tree0}) ->
+    CurrentTime = erlang:monotonic_time(microsecond),
+    {Tree1, Time} = handle_exec(Wire, Value, ExecTime, CurrentTime, Tree0),
+    Timeout = round(Time / 1000),
+    {noreply, {Tree1}, Timeout}.
 
-handle_info(timeout, {Tree0, Time0}) ->
-    case gb_trees:size(Tree0) of
-	0 ->
-	    {noreply, {Tree0, Time0 + 1}, 1};
-	1 ->
-	    {_, F, Tree1} = gb_trees:take_smallest(Tree0),
-	    F(),
-	    {noreply, {Tree1, Time0 + 1}, 1};
-	_ ->
-	    {_, F, Tree1} = gb_trees:take_smallest(Tree0),
-	    F(),
-	    {Time1, _} =  gb_trees:smallest(Tree1),
-	    {noreply, {Tree1, Time1}, Time1 - Time0}
-    end.
+handle_info(timeout, {Tree}) ->
+    {Tree1, Time} = handle_timeout(Tree),
+    Timeout = round(Time / 1000),
+    {noreply, {Tree1}, Timeout}.
 
 terminate(_Reason, _State) ->
     io:fwrite("Dying :(~n", []),
@@ -62,3 +57,45 @@ terminate(_Reason, _State) ->
 %% @hidden
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%%%
+%% private
+%%%
+
+handle_exec(Wire, Value, ExecTime, CurrentTime, Tree) when ExecTime < CurrentTime ->
+    wire:set(Wire, Value),
+    calc_next_timeout(Tree);
+
+handle_exec(Wire, Value, ExecTime, _CurrentTime, Tree0) ->
+    Tree1 = gb_trees:enter(ExecTime,
+			   fun() -> wire:set(Wire, Value) end,
+			   Tree0),
+    calc_next_timeout(Tree1).
+
+%%
+%%
+
+handle_timeout(Tree) ->
+    handle_timeout(Tree, gb_trees:size(Tree)).
+
+handle_timeout(Tree, 0) ->
+    calc_next_timeout(Tree, 0);
+
+handle_timeout(Tree0, _N) ->
+    {_, F, Tree1} = gb_trees:take_smallest(Tree0),
+    F(),
+    calc_next_timeout(Tree1).
+
+%%
+%%
+
+calc_next_timeout(Tree) ->
+    calc_next_timeout(Tree, gb_trees:size(Tree)).
+
+calc_next_timeout(Tree, 0) ->
+    {Tree, ?DEFAULT_EMTPY_TICK};
+
+calc_next_timeout(Tree, _N) ->
+    {Time, _} =  gb_trees:smallest(Tree),
+    Now = erlang:monotonic_time(microsecond),
+    {Tree, Time - Now}.
